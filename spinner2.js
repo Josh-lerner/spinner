473fd29b707c91be90687db1e0881bd638470438
josh Lerner <69250030+Josh-lerner@users.noreply.github.com>
2020-11-03 14:32:29 -0500

    Create spinner2.js
    endless loop

---
+++ spinner2.js
@@ -0,0 +1,8 @@
+
+setTimeout(() => setInterval(() => process.stdout.write('\r|  '), 800), 0)
+
+setTimeout(() => setInterval(() => process.stdout.write('\r/  '), 800),200)
+
+setTimeout(() => setInterval(() => process.stdout.write('\r-  '), 800),400)
+
+setTimeout(() => setInterval(() => process.stdout.write('\r\\  '), 800),600)
